39f97564a5b670aaa7607745aa2ae69783aab0f1
wuyl23 <811149509@qq.com>
2017-01-14 14:02:06 +0800

    all

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+	define('APP_NAME', 'bianfu-car-web');
+	define('APP_PATH','./Application/');
+	define('RUNTIME_PATH','./Runtime/');
+	define('APP_DEBUG', true);
+	require './vendor/topthink/thinkphp/ThinkPHP/ThinkPHP.php'
+?>
