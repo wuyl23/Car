7c9f56224e41477632980153dbeea5c1d23182ed
adminfly <961789212@qq.com>
2017-01-14 15:41:56 +0800

    数据库

--- carsearch.sql
+++ carsearch.sql
@@ -2,7 +2,7 @@ desc <table_name>
 show create table carsearch_fygg;
 CREATE TABLE `carsearch_fygg` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `anhao` varchar(50) DEFAULT NULL comment '案号',
+  `date` varchar(50) DEFAULT NULL comment '发布时间',
   `title` varchar(255) DEFAULT NULL comment '公告标题',
   `value` varchar(255) DEFAULT NULL comment '公告内容',
   `courtname` varchar(255) DEFAULT NULL comment '法院名称',
