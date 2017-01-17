1824466a4a8d501ded51479d9853a857a7b21b0a
adminfly <961789212@qq.com>
2017-01-17 09:46:16 +0800

    分页功能数据

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -5,11 +5,27 @@ use Think\Controller;
 
 class IndexController extends Controller
 {
-    public function index()
+    public function index(){
 
-    {
-       $list = M('fygg')->select();
-       $this->assign('list',$list);
+    	// echo "chenggong";
+       // $list = M('fygg')->limit(0,10)->select();
+       // $this->assign('list',$list);
        $this->display();
+    }
+
+
+
+    public function get (){
+    		$num = 1;
+    	if ($num <= 1) {
+    		$arr = 0;
+    	}else{
+    		$arr = $num*10;
+    	}
+
+    	$ret = M('fygg')->where()->limit($arr,10)->select();
+    	// var_dump($ret);
+    	$this -> ajaxreturn($ret);
+
     }
 }
